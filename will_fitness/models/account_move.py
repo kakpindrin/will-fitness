0cd8b0e61707b10f5548658f360bba0431114a80
Kavola <kavolad@gmail.com>
2021-07-09 19:12:47 +0000

    Merci Seigneur Éternel Notre DIEU pour tout.

--- will_fitness/models/account_move.py
+++ will_fitness/models/account_move.py
@@ -7,18 +7,36 @@ from odoo import api, fields, models, _
 class account_move(models.Model):
     _inherit = 'account.move'
 
+    facture_payee = fields.Boolean(string="Facture Payée ?", default=False, compute="_compute_facture_payee")
 
-    def action_register_payment(self):
-        state_of_payment = ['paid', 'in_payment']
-        #if self.payment_state in state_of_payment:
-        the_cards_domain = [('contact_id','=',self.partner_id.id)]
-        the_all_cards = self.env['hr.rfid.card'].search(the_cards_domain, order='id desc')
+    
+    @api.depends('payment_state')
+    def _compute_facture_payee(self):
+        for record in self:
+            state_of_payment = ['paid', 'in_payment']
+            if record.payment_state in state_of_payment:
+                record.facture_payee = True
+            if record.facture_payee:
+                the_cards_domain = [('contact_id','=',self.partner_id.id)]
+                the_all_cards = self.env['hr.rfid.card'].search(the_cards_domain, order='id desc')
 
-        if len(the_all_cards) > 0:
-            the_all_cards[0].write({
-                'deactivate_on': the_all_cards[0].activation_temp_date
-            })
-        result = super(account_move, self).action_register_payment()
+                if len(the_all_cards) > 0:
+                    the_all_cards[0].write({
+                        'deactivate_on': the_all_cards[0].activation_temp_date
+                    })
+    
+
+    # def action_register_payment(self):
+    #     state_of_payment = ['paid', 'in_payment']
+    #     #if self.payment_state in state_of_payment:
+    #     the_cards_domain = [('contact_id','=',self.partner_id.id)]
+    #     the_all_cards = self.env['hr.rfid.card'].search(the_cards_domain, order='id desc')
+
+    #     if len(the_all_cards) > 0:
+    #         the_all_cards[0].write({
+    #             'deactivate_on': the_all_cards[0].activation_temp_date
+    #         })
+    #     result = super(account_move, self).action_register_payment()
         
 
-        return result
+    #     return result
