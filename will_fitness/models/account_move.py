a9e527bf980eac947570469d8e24792fb90d8c58
Kavola <kavolad@gmail.com>
2021-07-09 17:29:56 +0000

    Merci Seigneur Éternel Notre DIEU pour tout.

--- will_fitness/models/account_move.py
+++ will_fitness/models/account_move.py
@@ -7,14 +7,18 @@ from odoo import api, fields, models, _
 class account_move(models.Model):
     _inherit = 'account.move'
 
-    @api.onchange('amount_residual')
-    def _onchange_my_payment_state(self):
-        #state_of_payment = ['paid', 'in_payment']
-        if self.amount_residual == 0:
+
+    def action_register_payment(self):
+        state_of_payment = ['paid', 'in_payment']
+        if self.payment_state in state_of_payment:
             the_cards_domain = [('contact_id','=',self.partner_id.id)]
             the_all_cards = self.env['hr.rfid.card'].search(the_cards_domain, order='id desc')
 
             if len(the_all_cards) > 0:
-                the_all_cards[0].sudo().write({
+                the_all_cards[0].write({
                     'deactivate_on': the_all_cards[0].activation_temp_date
                 })
+        result = super(account_move, self).action_register_payment()
+        
+
+        return result
