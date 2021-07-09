88beddc22913f4539f7f944a0f05632b03ab934f
Kavola <kavolad@gmail.com>
2021-07-09 17:36:43 +0000

    Merci Seigneur Éternel Notre DIEU pour tout.

--- will_fitness/models/account_move.py
+++ will_fitness/models/account_move.py
@@ -10,14 +10,14 @@ class account_move(models.Model):
 
     def action_register_payment(self):
         state_of_payment = ['paid', 'in_payment']
-        if self.payment_state in state_of_payment:
-            the_cards_domain = [('contact_id','=',self.partner_id.id)]
-            the_all_cards = self.env['hr.rfid.card'].search(the_cards_domain, order='id desc')
+        #if self.payment_state in state_of_payment:
+        the_cards_domain = [('contact_id','=',self.partner_id.id)]
+        the_all_cards = self.env['hr.rfid.card'].search(the_cards_domain, order='id desc')
 
-            if len(the_all_cards) > 0:
-                the_all_cards[0].write({
-                    'deactivate_on': the_all_cards[0].activation_temp_date
-                })
+        if len(the_all_cards) > 0:
+            the_all_cards[0].write({
+                'deactivate_on': the_all_cards[0].activation_temp_date
+            })
         result = super(account_move, self).action_register_payment()
         
 
