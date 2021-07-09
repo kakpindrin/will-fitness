478034a6a768ee28501ea371cc2c13192b70fe5b
Kavola <kavolad@gmail.com>
2021-07-09 15:31:30 +0000

    Merci Seigneur Éternel Notre DIEU pour tout.

--- will_fitness/models/account_move.py
+++ will_fitness/models/account_move.py
@@ -7,8 +7,23 @@ from odoo import api, fields, models, _
 class account_move(models.Model):
     _inherit = 'account.move'
 
-    def action_register_payment(self):
-        result = super(account_move, self).action_register_payment()
+    # def action_register_payment(self):
+    #     result = super(account_move, self).action_register_payment()
+    #     state_of_payment = ['paid', 'in_payment']
+    #     if self.payment_state in state_of_payment:
+    #         the_cards_domain = [('contact_id','=',self.partner_id.id)]
+    #         the_all_cards = self.env['hr.rfid.card'].search(the_cards_domain, order='id desc')
+
+    #         if len(the_all_cards) > 0:
+    #             the_all_cards[0].write({
+    #                 'deactivate_on': the_all_cards[0].activation_temp_date
+    #             })
+
+    #     return result
+
+    @api.onchange('payment_state')
+    def _onchange_payment_state(self):
+        # for thist in self:
         state_of_payment = ['paid', 'in_payment']
         if self.payment_state in state_of_payment:
             the_cards_domain = [('contact_id','=',self.partner_id.id)]
@@ -18,18 +33,3 @@ class account_move(models.Model):
                 the_all_cards[0].write({
                     'deactivate_on': the_all_cards[0].activation_temp_date
                 })
-
-        return result
-
-    # @api.onchange('payment_state')
-    # def _onchange_payment_state(self):
-    #     for thist in self:
-    #         state_of_payment = ['paid', 'in_payment']
-    #         if thist.payment_state in state_of_payment:
-    #             the_cards_domain = [('contact_id','=',thist.partner_id.id)]
-    #             the_all_cards = self.env['hr.rfid.card'].search(the_cards_domain, order='id desc')
-
-    #             if len(the_all_cards) > 0:
-    #                 the_all_cards[0].write({
-    #                     'deactivate_on': the_all_cards[0].activation_temp_date
-    #                 })
